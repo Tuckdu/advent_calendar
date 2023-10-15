0b2fe7114a09dddf297e8200d5689bd456916bbf
Tugdual Le Pen <tugduu@gmail.com>
2023-10-15 17:21:53 +0200

    Allow more riddle response grammar

--- src/data/riddles_2023.js
+++ src/data/riddles_2023.js
@@ -9,6 +9,72 @@ export default [
     },
     {
         day: 2,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 3,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 4,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 5,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 6,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 7,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 8,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 9,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 10,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 11,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 12,
+        img: rebus_ile_hoedic,
+        riddle: null,
+        response: "L'ile d'hoedic"
+    },
+    {
+        day: 13,
         img: null,
         riddle: "Quand on joue, il faut la rattraper. Quand on la tire, il faut l’éviter.",
         response: "La balle"
