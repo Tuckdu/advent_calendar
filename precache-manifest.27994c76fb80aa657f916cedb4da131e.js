94f5550090f2c271d76f6a5b93f15379c0d2e365
Tugdual Le Pen <tugduu@gmail.com>
2023-12-01 14:09:41 +0100

    Updates

---
+++ precache-manifest.27994c76fb80aa657f916cedb4da131e.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec88351941b06f1492517b3f334157a",
+    "url": "/advent_calendar/index.html"
+  },
+  {
+    "revision": "7b46135918cdff4dc237",
+    "url": "/advent_calendar/static/css/main.fbbe6a6f.chunk.css"
+  },
+  {
+    "revision": "7476c493883a6d5207dc",
+    "url": "/advent_calendar/static/js/2.581af08a.chunk.js"
+  },
+  {
+    "revision": "d3b9b20c582091a571839814cdbd2d0b",
+    "url": "/advent_calendar/static/js/2.581af08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b46135918cdff4dc237",
+    "url": "/advent_calendar/static/js/main.926a9f14.chunk.js"
+  },
+  {
+    "revision": "22d6b27efd802cf13869",
+    "url": "/advent_calendar/static/js/runtime-main.87335d4e.js"
+  },
+  {
+    "revision": "160da74180bb967d4c32a0ce4a80efd8",
+    "url": "/advent_calendar/static/media/bonhomme.160da741.png"
+  },
+  {
+    "revision": "ae5bab8953a4ad4d9f7ed3df8ffad85f",
+    "url": "/advent_calendar/static/media/buche.ae5bab89.png"
+  },
+  {
+    "revision": "8823d89bae53b80e6b93e29b104ea5eb",
+    "url": "/advent_calendar/static/media/calendrier.8823d89b.png"
+  },
+  {
+    "revision": "54a1d453ecae5d7731e113a7cb6f3b28",
+    "url": "/advent_calendar/static/media/christmas-font-webfont.54a1d453.woff"
+  },
+  {
+    "revision": "6c15649aafc252e4d9b4322d7f30302b",
+    "url": "/advent_calendar/static/media/christmas-font-webfont.6c15649a.woff2"
+  },
+  {
+    "revision": "0a7b3d3a178e0af978fdcc2f08b58533",
+    "url": "/advent_calendar/static/media/christmas_bg.0a7b3d3a.webp"
+  },
+  {
+    "revision": "f78f7abd95e7d817eb3dfc8a36a7028e",
+    "url": "/advent_calendar/static/media/claustrophobie.f78f7abd.webp"
+  },
+  {
+    "revision": "071d52c07383497012568a231f383cad",
+    "url": "/advent_calendar/static/media/dodu_svelte.071d52c0.webp"
+  },
+  {
+    "revision": "dba38cf1f4f9cee63ded2f07a2faef06",
+    "url": "/advent_calendar/static/media/failure.dba38cf1.mp3"
+  },
+  {
+    "revision": "46344b1b5b2027700f35d867e3a8a6a4",
+    "url": "/advent_calendar/static/media/failure_ouch.46344b1b.mp3"
+  },
+  {
+    "revision": "822b05bee603c45bca4f265f531dcb32",
+    "url": "/advent_calendar/static/media/flaque.822b05be.png"
+  },
+  {
+    "revision": "52193e6fd247b538ca07d2d8f0e8cc22",
+    "url": "/advent_calendar/static/media/gift_texture1.52193e6f.jpg"
+  },
+  {
+    "revision": "3ddda2a647ceab2b97b247fe6fea1bd4",
+    "url": "/advent_calendar/static/media/gift_texture2.3ddda2a6.jpg"
+  },
+  {
+    "revision": "c54797a57a7ac72132582ff7890abddb",
+    "url": "/advent_calendar/static/media/gift_texture3.c54797a5.jpg"
+  },
+  {
+    "revision": "a928bb9e0324cf6bd939fec64a72a78f",
+    "url": "/advent_calendar/static/media/gift_texture4.a928bb9e.jpg"
+  },
+  {
+    "revision": "58aa922a887a126da793bb9587a323c6",
+    "url": "/advent_calendar/static/media/gift_texture5.58aa922a.jpg"
+  },
+  {
+    "revision": "e98bab6de36f78cbacd802942ef24611",
+    "url": "/advent_calendar/static/media/gift_texture6.e98bab6d.jpg"
+  },
+  {
+    "revision": "bef14a50969612fa8a7c72f27d045ac0",
+    "url": "/advent_calendar/static/media/gift_texture7.bef14a50.jpg"
+  },
+  {
+    "revision": "3493384898c11df16435d5b7049d3bf3",
+    "url": "/advent_calendar/static/media/icicle.34933848.svg"
+  },
+  {
+    "revision": "eac4529a86dc1445322b9c4a626050a7",
+    "url": "/advent_calendar/static/media/lock.eac4529a.svg"
+  },
+  {
+    "revision": "3cc1c24994dd81d63f334056b1d5686f",
+    "url": "/advent_calendar/static/media/pas_sage.3cc1c249.png"
+  },
+  {
+    "revision": "2e8b68a2d8f5484d1adb362dd375a28f",
+    "url": "/advent_calendar/static/media/sapin_boules.2e8b68a2.webp"
+  },
+  {
+    "revision": "4df33e0552152863aed1a788e672679b",
+    "url": "/advent_calendar/static/media/snowy_christmas_-_personal_use-webfont.4df33e05.woff2"
+  },
+  {
+    "revision": "c787f73bf26ff8bdc8fa69d6af7beaa3",
+    "url": "/advent_calendar/static/media/snowy_christmas_-_personal_use-webfont.c787f73b.woff"
+  },
+  {
+    "revision": "ddd96145c1634bfdcc56175eef6a29f9",
+    "url": "/advent_calendar/static/media/success.ddd96145.mp3"
+  },
+  {
+    "revision": "00654d7f50fd80f5d18168473ee37a08",
+    "url": "/advent_calendar/static/media/success_woohoo.00654d7f.mp3"
+  },
+  {
+    "revision": "a8122c97579470ccc4eee14652e8d206",
+    "url": "/advent_calendar/static/media/sucre_dorge.a8122c97.png"
+  },
+  {
+    "revision": "8c8f5096e52be36929279de33f14baa8",
+    "url": "/advent_calendar/static/media/the_perfect_christmas-webfont.8c8f5096.woff"
+  },
+  {
+    "revision": "bb580d5b5b9f88d60e8410130cd04910",
+    "url": "/advent_calendar/static/media/the_perfect_christmas-webfont.bb580d5b.woff2"
+  }
+]);
